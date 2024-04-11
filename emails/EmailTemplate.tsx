828fe85e94a57a7655347b14d16e7a373a412364
Vlad <vlasisla1988@gmail.com>
2024-04-11 13:44:59 +0300

    fix/template-rendering: Added automatization for creating html email template

--- emails/EmailTemplate.tsx
+++ emails/EmailTemplate.tsx
@@ -1,4 +1,4 @@
-import { Container, Html, Body } from "@react-email/components";
+import { Container, Html, Body, Preview } from "@react-email/components";
 import * as React from "react";
 import { EmailHead } from "./_components/_base/_EmailHead/EmailHead";
 import { Header } from "./_components/_shared/_Header/Header";
@@ -7,10 +7,11 @@ import { MainInfo } from "./_components/_shared/_MainInfo/MainInfo";
 import { AppInfo } from "./_components/_shared/_AppInfo/AppInfo";
 import { Footer } from "./_components/_shared/_Footer/Footer";
 
-export default function Email() {
+export const EmailTemplate = () => {
   return (
     <Html lang="en" dir="ltr">
       <EmailHead />
+      <Preview>This is the Best app for you 😉</Preview>
       <Body style={bodyStyles}>
         <Container style={containerStyles}>
           <Header />
@@ -21,7 +22,9 @@ export default function Email() {
       </Body>
     </Html>
   );
-}
+};
+
+export default EmailTemplate;
 
 const bodyStyles: StylesType = {
   width: "100%",
